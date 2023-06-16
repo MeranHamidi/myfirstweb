f510be3f419604b0f8979a678047ef8e719311a3
Mehran Hamidi <136412620+MeranHamidi@users.noreply.github.com>
2023-06-16 09:56:28 +0330

    test2.py

--- test2.py
+++ test2.py
@@ -8,19 +8,13 @@ with left_column:
     st.write('You Inter :', option)
 # Or even better, call Streamlit functions inside a "with" block:
 with right_column:
-    aa = fpy.Get_Price_History(
-        stock=option,
-        start_date='1402-01-01',
-        end_date='1402-03-23',
-        ignore_date=False,
-        adjust_price=False,
-        show_weekday=False,
-        double_date=False)
-    aa = aa.drop(columns=['Ticker', 'Name', 'Market'])
-    st.dataframe(aa)
-    st.line_chart( aa , y =['Close'], use_container_width = True)
-    st.bar_chart(aa['Value'], use_container_width = True)
+    #aa = fpy.Get_Price_History(stock=option,start_date='1402-01-01',end_date='1402-03-23',ignore_date=False, adjust_price=False,show_weekday=False, double_date=False)
+    #aa = aa.drop(columns=['Ticker', 'Name', 'Market'])
+    #st.dataframe(aa)
+    #st.line_chart( aa , y =['Close'], use_container_width = True)
+    #st.bar_chart(aa['Value'], use_container_width = True)
     #ast = st.dataframe(aa.style.highlight_max(axis=0, color='red'))
     #st.write(aa.keys())
     #st.write(aa.index)
+    st.write('hello')
 
