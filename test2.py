bd7a6a609c2e5b7cfc3cbfd425209e67d8826b35
Mehran Hamidi <136412620+MeranHamidi@users.noreply.github.com>
2023-06-17 11:54:04 +0330

    test2.py

--- test2.py
+++ test2.py
@@ -11,9 +11,9 @@ with right_column:
     aa = fpy.Get_Price_History(stock=option,start_date='1402-01-01',end_date='1402-03-23',ignore_date=False, adjust_price=False,show_weekday=False, double_date=False)
     aa = aa.drop(columns=['Ticker', 'Name', 'Market'])
     st.dataframe(aa)
-    st.line_chart( aa , y =['Close'], use_container_width = True)
+    st.line_chart( aa , y = ['Close'], use_container_width = True)
     st.bar_chart(aa['Value'], use_container_width = True)
-    ast = st.dataframe(aa.style.highlight_max(axis=0, color='red'))
+    #ast = st.dataframe(aa.style.highlight_max(axis=0, color='red'))
     #st.write(aa.keys())
     #st.write(aa.index)
    
